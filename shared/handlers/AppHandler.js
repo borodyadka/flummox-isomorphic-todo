0797fe912a4de3b21a54777550fda50a5d73d572
Pavel Kilin <pavel@asdf404.net>
2015-04-29 07:20:16 +0600

    Removed "edit" from footer

--- shared/handlers/AppHandler.js
+++ shared/handlers/AppHandler.js
@@ -9,7 +9,6 @@ class AppHandler extends React.Component {
                     <RouteHandler {...this.props} key={this.props.pathname} />
                 </section>
                 <footer className="info">
-                    <p>Double-click to edit a todo</p>
                     <p>Template by <a href="http://sindresorhus.com">Sindre Sorhus</a></p>
                     <p>Created by <a href="http://todomvc.com">you</a></p>
                     <p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
